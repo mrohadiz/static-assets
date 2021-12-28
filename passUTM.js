87090af3eaa8755d18b6041022e45a34135cf74e
zaka gimat <zakagimat@users.noreply.github.com>
2021-12-28 08:35:40 +0700

    update skipp #
    update skit link dengan #

--- passUTM.js
+++ passUTM.js
@@ -1,15 +1,36 @@
-(function() {
-    const params = new URLSearchParams(window.location.search);
-    const utm_params = [];
-    params.forEach(function(value, key) {
-        if (key.startsWith('utm_')) {
-            utm_params.push(key+'='+value)
-        }
-    })
+const utmParams = [
+  'utm_source',
+  'utm_medium',
+  'utm_campaign',
+  'utm_content',
+  'utm_term'
+];
+
+ const utm_params = [];
+utmParams.forEach(param => {
+
+ const params = Cookies.get(param);
+if(params=='undefined'){
+console.log(param+' Kosong')
+}else{
+utm_params.push(param+'='+params)
+}
+
+ console.log(param+':'+params);
+});
+ // var pValue = getUTMValue(param);
+
+//const params = new URLSearchParams(window.location.search);
+   
+  
+    var href = jQuery(this).attr('href');
+
     utm_search = utm_params.join('&');
     if (!!utm_search) {
         document.querySelectorAll('a[href]').forEach(function(ele, idx) {
-            ele.href = ele.href + (ele.href.indexOf('?') === -1 ? '?' : '&') + utm_search;
+            if(!ele.href.match(/\#/)){
+             ele.href = ele.href + (ele.href.indexOf('?') === -1 ? '?' : '&') + utm_search;
+             }
+           
         });
     }
-})();
